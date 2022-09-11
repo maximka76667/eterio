dca2d7a15d84f93110e9a496e83494aa1833d635
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-09-11 22:45:06 +0600

    home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,10 @@ import "./Home.sass"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1 className='home__title'>Welcome to Alcopedia</h1>
+      <p className='home__text'>Here you can find ingredients and proportions for cocktails' preparing</p>
+    </div>
   )
 }
 
