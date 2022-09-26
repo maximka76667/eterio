d11b260fba794fd8280ae04f3b382a6467b84e6c
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-09-26 10:28:43 +0200

    input and buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Header.sass';
 import logo from '../../images/logo512.png';
@@ -6,6 +6,16 @@ import { HeaderProps } from '../../interfaces';
 
 const Header = ({ closeSidebar, isSidebarOpened }: HeaderProps) => {
   const { pathname } = useLocation();
+
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleLogin = () => {
+    console.log(email);
+  };
 
   return (
     <header className='header'>
@@ -22,6 +32,17 @@ const Header = ({ closeSidebar, isSidebarOpened }: HeaderProps) => {
           Alcopedia
         </p>
       )}
+      <div className='header__buttons'>
+        <input
+          name='email'
+          type='text'
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <button className='header__login' onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </header>
   );
 };
