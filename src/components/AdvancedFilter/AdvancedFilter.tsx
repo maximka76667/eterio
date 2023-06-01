b0c2f9785a997b84f8489aafd6cd7cba2a8aea48
Maxim <74974283+maximka76667@users.noreply.github.com>
2023-06-01 13:45:43 +0200

    change logo text, hash advaned filter labels

--- src/components/AdvancedFilter/AdvancedFilter.tsx
+++ src/components/AdvancedFilter/AdvancedFilter.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { CategoriesContext } from '../../contexts';
 import AdvamcedFilterProps from './AdvamcedFilterProps';
+import { v4 as uuidv4 } from 'uuid';
 
 const AdvancedFilter = ({ onChange }: AdvamcedFilterProps) => {
   const categories = useContext(CategoriesContext);
+
+  const hash: string = uuidv4();
 
   const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
 
@@ -31,23 +34,26 @@ const AdvancedFilter = ({ onChange }: AdvamcedFilterProps) => {
       >
         <h3 className='ff-montse text-lg'>Category</h3>
         <div className='w-4/5 mx-auto grid grid-cols-2 gap-1 mt-2'>
-          {categories.map(({ id, name }) => (
-            <div key={id} className='flex items-center'>
-              <input
-                id={name}
-                type='checkbox'
-                value={name}
-                className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-                onChange={(event) => onChange(event, name)}
-              />
-              <label
-                htmlFor={name}
-                className='ml-2 text-sm ff-montse text-gray-900 dark:text-gray-300'
-              >
-                {name}
-              </label>
-            </div>
-          ))}
+          {categories.map(({ id, name }) => {
+            const hashedName = `${name}-${hash}`;
+            return (
+              <div key={id} className='flex items-center'>
+                <input
+                  id={hashedName}
+                  type='checkbox'
+                  value={name}
+                  className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+                  onChange={(event) => onChange(event, name)}
+                />
+                <label
+                  htmlFor={hashedName}
+                  className='ml-2 text-sm ff-montse text-gray-900 dark:text-gray-300'
+                >
+                  {name}
+                </label>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,8 +4,8 @@ import logo from '../../images/logo512.png';
 const Logo = () => {
   return (
     <>
-      <img className='header__logo-img' src={logo} alt='Alcopedia' />
-      <p className='ff-montse text-2xl hidden md:block'>Alcopedia</p>
+      <img className='header__logo-img' src={logo} alt='Eterio' />
+      <p className='ff-montse text-2xl hidden md:block'>Eterio</p>
     </>
   );
 };
